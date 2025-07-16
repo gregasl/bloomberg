f1c76ab8a631e6da42ca51a624b8df71dded88ef
greg.mahoney <greg.mahoney@aslcap.com>
2025-07-16 08:51:05 -0400

    change to nvar

--- sql/bloomberg_sec_id.sql
+++ sql/bloomberg_sec_id.sql
@@ -8,7 +8,7 @@ END
 GO
 
 CREATE TABLE bloomberg_sec_id (
-   sec_id_type NCHAR(4) NOT NULL CONSTRAINT bbg_sec_id_sec_id_type_check CHECK (sec_id_type in ('FUT ', 'MBS ', 'TSY ')),
+   sec_id_type NVARCHAR(8) NOT NULL CONSTRAINT bbg_sec_id_sec_id_type_check CHECK (sec_id_type in ('FUT', 'MBS', 'TSY')),
    sec_id NVARCHAR(32) NOT NULL,
    ts DATETIME2 DEFAULT GETDATE(),
    PRIMARY KEY(sec_id, sec_id_type)
